df62e1f22a6402b828b8141f4bf7a65bc64508da
Kaoru-D <daniel.rt5d@gmail.com>
2024-04-01 18:54:07 -0500

    estilado de twitter y escalabilidad

---
+++ rct/primer-ejercicio/ejercicio/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+export function App() {
+    return( 
+    <TwitterFollowCard userName="Daniel_Bleach16" name="Daniel P"/> 
+        )
+   
+}
