954729fa21f311e0488d353f573c3e48f7612465
Kaoru-D <daniel.rt5d@gmail.com>
2024-04-01 19:01:54 -0500

    organización de la escalabilidad

--- rct/primer-ejercicio/ejercicio/src/App.jsx
+++ rct/primer-ejercicio/ejercicio/src/App.jsx
@@ -2,7 +2,12 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 export function App() {
     return ( 
-    <TwitterFollowCard userName="Daniel_Bleach16" name="Daniel P"/> 
+        <>
+        <TwitterFollowCard userName="Daniel_Bleach16" name="Daniel P"/> 
+        <TwitterFollowCard userName="suzzy_biosix" name="Suzana Biosix"/>
+        <TwitterFollowCard userName="Tiffani_Time" name="Tiffani"/>  
+        </>
+    
     )
    
 }
