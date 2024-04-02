7b61c59eaa49d48242e5d85e5e6b03e5d043663e
Kaoru-D <daniel.rt5d@gmail.com>
2024-04-01 19:21:21 -0500

    agregoel simbolo de concatenación

--- rct/primer-ejercicio/ejercicio/src/App.jsx
+++ rct/primer-ejercicio/ejercicio/src/App.jsx
@@ -3,9 +3,7 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 export function App() {
     return ( 
         <>
-        <TwitterFollowCard userName="Daniel_Bleach16" name="Daniel P"/> 
-        <TwitterFollowCard userName="suzzy_biosix" name="Suzana Biosix"/>
-        <TwitterFollowCard userName="Tiffani_Time" name="Tiffani"/>  
+        <TwitterFollowCard userName="Daniel_Bleach16" name="Daniel P"/>  
         </>
     
     )
--- rct/primer-ejercicio/ejercicio/src/TwitterFollowCard.jsx
+++ rct/primer-ejercicio/ejercicio/src/TwitterFollowCard.jsx
@@ -1,10 +1,12 @@
 export function TwitterFollowCard({userName,name,isFollowing}){
-  return(  
+  /* const imageSrc ='https://unavatar.io/twitter/${userName}' */
+    return(  
     <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img className='tw-followCard-avatar' 
-                src={"https://unavatar.io/twitter/{userName}"} 
-                alt={"Avatar de {name}"}/>
+                /* src={imageSrc}  */
+                src={'https://unavatar.io/twitter/${userName}'}
+                alt="Avatar de usuario"/>
                 <div className='tw-followCard-info'>
                     <strong>{name}</strong>
                     <span className='tw-followCard-userName'>@{userName}</span>
