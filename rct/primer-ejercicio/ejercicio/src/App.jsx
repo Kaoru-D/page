354b6a88c3b8b7a002e9a047fbdced88156df377
Kaoru-D <daniel.rt5d@gmail.com>
2024-04-09 06:05:26 -0500

    manejo de optimizacion de arrays con el primer fragment, botón interactivo deguir agregando los estilos CSS respectivos al botón

--- rct/primer-ejercicio/ejercicio/src/App.jsx
+++ rct/primer-ejercicio/ejercicio/src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 export function App() {
-    return ( 
+
+    const danielP={isFollowing:true,userName:'Daniel_Bleach16'}
+    return (
         <>
-        <TwitterFollowCard userName="Daniel_Bleach16" name="Daniel P"/>  
+            <TwitterFollowCard {...danielP} >
+            Daniel P
+            </TwitterFollowCard>
+            <TwitterFollowCard isFollowing={false} userName="daniel.pulagrin" >
+            Daniel Pulgarin
+            </TwitterFollowCard>
+            <TwitterFollowCard isFollowing={false} userName="kaitgonewild" >
+            Kait Wild
+            </TwitterFollowCard>
+            <TwitterFollowCard isFollowing userName="onlineinsane" >
+            Insane Content
+            </TwitterFollowCard>
         </>
-    
+
     )
-   
+
 }
--- rct/primer-ejercicio/ejercicio/src/TwitterFollowCard.jsx
+++ rct/primer-ejercicio/ejercicio/src/TwitterFollowCard.jsx
@@ -1,22 +1,32 @@
-export function TwitterFollowCard({userName,name,isFollowing}){
-  /* const imageSrc ='https://unavatar.io/twitter/${userName}' */
-    return(  
-    <article className='tw-followCard'>
+import { useState } from 'react'
+
+export function TwitterFollowCard({ userName = '#', children/*name*/}) {
+    const [isFollowing, serIsFollowing] = useState(false)
+
+    /* const imageSrc = 'https://unavatar.io/twitter/${userName}'; */
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+    const handleClick = () => {
+        serIsFollowing(!isFollowing)
+    }
+    return (
+        <article className='tw-followCard'>
             <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' 
-                /* src={imageSrc}  */
-                src={'https://unavatar.io/twitter/${userName}'}
-                alt="Avatar de usuario"/>
+                <img
+                    className='tw-followCard-avatar'
+                    /* src={imageSrc} */
+                    src={`https://unavatar.io/${userName}`}
+                    alt='Avatar de usuario'
+                />
                 <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
+                    <strong>{children}</strong>
                     <span className='tw-followCard-userName'>@{userName}</span>
                 </div>
             </header>
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
+
+                <button className={buttonClassName} onClick={handleClick}>{text}</button>
             </aside>
-    </article>
-  )
+        </article>
+    );
 }
