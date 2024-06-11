df79e869c6f4906e098ae602aade9e2d0678b7c3
Kaoru-D <daniel.rt5d@gmail.com>
2024-06-11 15:51:04 -0500

    prueba con nmp file-saver

--- rct/02-tricky/src/logic/storage/index.js
+++ rct/02-tricky/src/logic/storage/index.js
@@ -1,9 +1,14 @@
-export const saveGameToStorage=({board,turn})=>{
-    windows.localStorage.setItem('board',JSON.stringify(board))
-    windows.localStorage.setItem('turn',turn)
+import FileSaver from "file-saver"
+
+export const saveGameToStorage = ({ board, turn }) => {
+    var file = new File([('board', JSON.stringify(board))], "tablero.txt" ,{ type: "text/plain;charset=utf-8" })
+    FileSaver.saveAs(file)
+    var file = new File([('turn', turn)], "turno.txt", { type: "text/plain;charset=utf-8" })
+    FileSaver.saveAs(file)/* var file = new File(["Hello, world!"], "hello world.txt", {type: "text/plain;charset=utf-8"});
+    FileSaver.saveAs(file); */
 }
 
-export const resetGameStorage=()=>{
+export const resetGameStorage = () => {
     window.localStorage.removeItem('board')
     window.localStorage.removeItem('turn')
-}
+}/*  */
