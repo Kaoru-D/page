95a63b265d192be758f9f585f00884446541f9a4
Kaoru-D <daniel.rt5d@gmail.com>
2024-07-10 18:02:38 -0500

    Regeneración de todo el proyecto

--- rct/03-raton/src/App.jsx
+++ rct/03-raton/src/App.jsx
@@ -1,60 +1,60 @@
 import { useEffect, useState } from "react"
 
-const followMouse = () => {
-  return (<>
-    <div style={{
-      position: 'absolute',
-      backgroudColor: 'rgba(0,0,0,0.5)',
-      border: '2px solid #222',
-      borderRadius: '50%',
-      opacity: '0.8',
-      pointerEvents: 'none',
-      left: -20,
-      top: -20,
-      width: 40,
-      height: 40,
-      transform: `translate(${position.x} , ${position.y})`
-    }} />
-
-
-    <button onClick={() => setEnabled(!enabled)} >
-      {enabled ? 'Desactivar' : 'Activar'} seguimiento
-    </button>
-  </>
-  )
-}
-
-function App() {
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
-  const [mounted, setMounted] = useEffect(true)
+  
 
   useEffect(() => {
-    console.log('effect', { enabled })
-
+    console.log('efecto', { enabled })
     const handleMove = (event) => {
-      const { clienteX, clienteY } = event
-      console.log('handleMove', { clienteX, clienteY })
-      setPosition({ x: clienteX, y: clienteY })
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
     }
-
     if (enabled) {
       window.addEventListener('pointermove', handleMove)
     }
     return () => {
+      console.log('cleanup')
       window.removeEventListener('pointermove', handleMove)
     }
+
   }, [enabled])
   return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgb(0 0 1)',
+        border: '2px solid rgb(255 255 255)',
+        borderRadius: '50%',
+        opacity: '0.8',
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px,${position.y}px)`
+      }}
+      />
 
-    <main>
-      {/* <h3>Complejo del Ratón</h3> */}
-      <followMouseouse />
-    </main>
 
-
-
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} Seguimiento
+      </button>
+    </>
   )
+}
+
+function App() {
+  
+  return (
+    <main>
+      <FollowMouse />
+      
+    </main>
+  )
+
 }
 
 export default App
